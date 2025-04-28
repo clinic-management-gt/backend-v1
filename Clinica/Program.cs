39595f3528b21aa6acd63ed5dad2eea4fa66b765
ecarcamo <estebancarcamou@gmail.com>
2025-04-28 10:56:10 -0600

    delete trash code

--- Clinica/Program.cs
+++ Clinica/Program.cs
@@ -18,11 +18,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", 
-    "Balmy", "Hot", "Sweltering", "Scorching"
-};
+
 
 var app = builder.Build();
 
@@ -37,29 +33,13 @@ app.UseHttpsRedirection();
 
 
 app.UseCors("AllowFrontend");
+// Ejemplo de endpoint básico
+app.MapGet("/ping", () => Results.Json(new {message = "pong"}) );
+
 
 // Mapea controladores como /pacientes, /testdb
 app.MapControllers();
 
-// Ejemplo de endpoint básico
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
