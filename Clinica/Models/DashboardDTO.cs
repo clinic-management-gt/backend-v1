e086eeb9308faec62f76b8a3a32d02c0041c1d72
HUG0BA <bar23556@uvg.edu.gt>
2025-05-08 21:37:58 -0600

    Added dashaboard endpoint
    Dashboard shows all of the appointments of the current day
    /dashboard
    /dashboard?status=validStatus

--- Clinica/Models/DashboardDTO.cs
+++ Clinica/Models/DashboardDTO.cs
@@ -2,18 +2,12 @@
 namespace Clinica.Models;
 public class DashBoardDTO
 {
-    public string PatientName { get; set; }
-    public string DoctorName { get; set; }
+    public string PatientName { get; set; } = string.Empty;
+    public string DoctorName { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
     public DateTime Date { get; set; }
-    public string Status { get; set; }
 
-    public DashBoardDTO(string patientName, string doctorName, DateTime date, string status)
-    {
-       PatientName = patientName;
-       DoctorName = doctorName;
-       Date = date;
-       Status = status;
-    }
+    
 }
 
 
