26c14407c43428a6691fd15b378f6e2b85cd86e1
Nesstor07 <erneram1975@gmail.com>
2025-05-12 19:14:50 -0600

    CitasController updated, today patients deleted

--- Clinica/Controllers/CitasController.cs
+++ Clinica/Controllers/CitasController.cs
@@ -59,45 +59,5 @@ namespace Clinica.Controllers
             return StatusCode(500, $"Error al consultar las citas: {ex.Message}");
          }
       }
-      // GET: /citas/pacientes/today
-      [HttpGet("pacientes/today")]
-      public IActionResult GetCitasForToday(){
-         string? connectionString = _config.GetConnectionString("DefaultConnection");
-         try 
-         {
-            using var conn = new NpgsqlConnection(connectionString);
-            conn.Open();
-            var citasToday = new List<object>();
-            DateTime today = DateTime.Today;
-            DateTime tomorrow = today.AddDays(1);
-            var sql = @"
-               SELECT id, appointment_date, reason, status, created_at, updated_at
-               FROM appointments
-               WHERE appointment_date >= @today AND appointment_date < @tomorrow";
-            using var cmd = new NpgsqlCommand(sql, conn);
-            cmd.Parameters.AddWithValue("today", today);
-            cmd.Parameters.AddWithValue("tomorrow", tomorrow);
-
-            using var reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-               citasToday.Add(new
-               {
-                  Id = reader.GetInt32(0),
-                  AppointmentDate = reader.GetDateTime(1),
-                  Reason = reader.GetString(2),
-                  Status = reader.GetString(3),
-                  CreatedAt = reader.GetDateTime(4),
-                  UpdatedAt = reader.IsDBNull(5) ? (DateTime?)null: reader.GetDateTime(5)
-               });
-            }
-            return Ok(citasToday);
-         }
-         catch(Exception ex)
-         {
-            Console.WriteLine($"❌ Error al consultar citas: {ex.Message}");
-            return StatusCode(500, $"Error al consultar las citas: {ex.Message}");
-         }
-      }
    }
 }
