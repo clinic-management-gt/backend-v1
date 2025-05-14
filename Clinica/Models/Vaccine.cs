895b5d95773d82d5a9cc518b8cf82b14e1125703
ecarcamo <estebancarcamou@gmail.com>
2025-05-13 18:26:10 -0600

    feature: post and patch of a vaccine

---
+++ Clinica/Models/Vaccine.cs
@@ -0,0 +1,9 @@
+namespace Clinica.Models {
+    public class Vaccine {
+        public int Id {get;set;}
+        public string Name {get;set;} = string.Empty;
+        public string Brand { get;set;} = string.Empty;
+        public DateTime CreatedAt { get;set;}
+        public DateTime? UpdatedAt {get;set;}
+    }
+}
