dfa554dc758b12e463b54c42e156ce8af4e5dee6
ecarcamo <estebancarcamou@gmail.com>
2025-05-14 12:40:40 -0600

    fix: cors for the correct url

--- Clinica/Program.cs
+++ Clinica/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5176")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
