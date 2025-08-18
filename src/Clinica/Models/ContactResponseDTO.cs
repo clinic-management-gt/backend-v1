d07d332ba49059c2fb0568e5c06c5086b82df131
Nesstor07 <ernestoascencio2005@gmail.com>
2025-08-18 12:48:37 -0600

    Required modifier added to ContactResponseDTO

--- src/Clinica/Models/ContactResponseDTO.cs
+++ src/Clinica/Models/ContactResponseDTO.cs
@@ -4,9 +4,9 @@ namespace Clinica.Models
     {
         public int Id { get; set; }
         public int PatientId { get; set; }
-        public string Type { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public required string Type { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
         public List<PhoneResponseDTO> Phones { get; set; } = new();
     }
 
