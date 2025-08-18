8f942690ebb35a24238d2aaa27159d8dc2c4663d
Nesstor07 <ernestoascencio2005@gmail.com>
2025-08-18 12:59:42 -0600

    Better usage of Required modifier

--- src/Clinica/Models/ContactResponseDTO.cs
+++ src/Clinica/Models/ContactResponseDTO.cs
@@ -2,17 +2,17 @@ namespace Clinica.Models
 {
     public class ContactResponseDTO
     {
-        public int Id { get; set; }
-        public int PatientId { get; set; }
+        public required int Id { get; set; }
+        public required int PatientId { get; set; }
         public required string Type { get; set; }
         public required string Name { get; set; }
         public required string LastName { get; set; }
-        public List<PhoneResponseDTO> Phones { get; set; } = new();
+        public required List<PhoneResponseDTO> Phones { get; set; }
     }
 
     public class PhoneResponseDTO
     {
-        public int ContactId { get; set; }
+        public required int ContactId { get; set; }
         public required string PhoneNumber { get; set; }
     }
 }
