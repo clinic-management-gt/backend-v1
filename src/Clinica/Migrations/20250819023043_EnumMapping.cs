3e1988a68bed964f5817f9c8cb31048cf3ccb5ec
BARHUG0 <bar23556@uvg.edu.gt>
2025-08-18 20:43:12 -0600

    Enum mapping migration

---
+++ src/Clinica/Migrations/20250819023043_EnumMapping.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clinica.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnumMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /*
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "treatments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<AppointmentStatus>(
+                name: "status",
+                table: "appointments",
+                type: "appointment_status_enum",
+                nullable: false,
+                defaultValue: AppointmentStatus.Confirmado);
+                */
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            /*
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "treatments");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "appointments");
+                */
+        }
+    }
+}
