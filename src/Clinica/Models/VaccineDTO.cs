ed4fedfd464ed4d088435c45aa7449aaa4fe94d0
BARHUG0 <bar23556@uvg.edu.gt>
2025-08-19 19:16:37 -0600

    Fixed vaccine and keys comments

--- src/Clinica/Models/VaccineDTO.cs
+++ src/Clinica/Models/VaccineDTO.cs
@@ -4,8 +4,8 @@ namespace Clinica.Models
     public class VaccineDTO
     {
         public int? Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Brand { get; set; } = string.Empty;
+        public required string Name { get; set; }
+        public required string Brand { get; set; }
 
     }
 }
