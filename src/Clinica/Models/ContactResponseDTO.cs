16cfe7c07352c97270ea2e0f19132e7ed0832fe1
BARHUG0 <bar23556@uvg.edu.gt>
2025-10-08 12:18:19 -0600

    Updated DTO and endpoint

--- src/Clinica/Models/ContactResponseDTO.cs
+++ src/Clinica/Models/ContactResponseDTO.cs
@@ -1,18 +1,26 @@
-namespace Clinica.Models
-{
-    public class ContactResponseDTO
-    {
-        public required int Id { get; set; }
-        public required int PatientId { get; set; }
-        public required string Type { get; set; }
-        public required string Name { get; set; }
-        public required string LastName { get; set; }
-        public required List<PhoneResponseDTO> Phones { get; set; }
-    }
+namespace Clinica.Models;
 
-    public class PhoneResponseDTO
-    {
-        public required int ContactId { get; set; }
-        public required string PhoneNumber { get; set; }
-    }
+public class ContactResponseDTO
+{
+    public required int Id { get; set; }
+    public required int PatientId { get; set; }
+    public required string Type { get; set; }
+    public required string Name { get; set; }
+    public required string LastName { get; set; }
+    public required List<PhoneResponseDTO> Phones { get; set; }
+    public required List<EmailDTO> Emails { get; set; }
 }
+
+public class PhoneResponseDTO
+{
+    public required int ContactId { get; set; }
+    public required string PhoneNumber { get; set; }
+}
+
+public class EmailDTO
+{
+    public required int EmailId { get; set; }
+    public required string Email { get; set; }
+}
+
+
